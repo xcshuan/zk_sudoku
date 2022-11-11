659a649beaa4340672140c2173d0490dddcdf5ab
xcshuan <xcshuan@hotmail.com>
2022-11-11 19:28:07 +0800

    add basic circom circuit

---
+++ others/circom/contracts/hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-gas-reporter");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+  gasReporter: {
+    enabled: true,
+  }
+};
